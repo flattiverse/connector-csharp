dd23c413d9e2480903bb35963e1c9544c30875b7
Tooasty77 <123006281+Tooasty77@users.noreply.github.com>
2023-01-19 12:42:25 +0100

    Added Universe and UniverseGroup

---
+++ Connector/Connector/Universe.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Text.Json;
+using System.Xml.Linq;
+
+namespace Flattiverse
+{
+    public class Universe
+    {
+        private Connection connection;
+
+        private UniverseGroup universeGroup;
+
+        public readonly short ID;
+
+
+        internal Universe(UniverseGroup universeGroup, Connection connection, short ID) 
+        { 
+            this.universeGroup = universeGroup;
+            this.connection = connection;
+            this.ID = ID;
+        }
+
+        public async Task Create(string unitJson)
+        {
+            await createUpdateUnit("CreateUnit", unitJson);
+        }
+
+        public async Task Update(string unitJson)
+        {
+            await createUpdateUnit("UpdateUnit", unitJson);
+        }
+
+        private async Task createUpdateUnit(string command, string unitJson)
+        {
+            using (Block block = connection.blockManager.GetBlock())
+            {
+                Packet packet = new Packet(block.Id);
+                packet.Command = command;
+
+                CommandParameter param = new CommandParameter("data");
+
+                try
+                {
+                    JsonDocument.Parse(unitJson, Connection.jsonDocOptions);
+                }
+                catch
+                {
+                    throw new ArgumentException($"Json format invalid.", nameof(unitJson));
+                }
+
+                string data;
+
+                using (MemoryStream ms = new MemoryStream())               
+                {
+                    using (Utf8JsonWriter writer = new Utf8JsonWriter(ms, Connection.jsonOptions))
+                    {
+                        writer.WritePropertyName("data");
+
+                        writer.WriteStartObject();
+
+                        writer.WriteNumber("universe", ID);
+
+                        writer.WritePropertyName("unit");
+
+                        writer.WriteRawValue(unitJson);
+
+                        writer.WriteEndObject();         
+                    }
+
+                    ms.Position = 0;
+                    data = new StreamReader(ms).ReadToEnd();
+                }
+
+                param.SetJsonValue(data);
+
+                packet.Parameters.Add(param);
+
+                await connection.SendCommand(packet);
+
+                await block.Wait();
+
+                Packet? responsePacket = block.Packet;
+
+                //ResponsePacket lesen
+            }
+        }
+
+        public async Task Delete(string name)
+        {
+            using (Block block = connection.blockManager.GetBlock())
+            {
+                Packet packet = new Packet(block.Id);
+                packet.Command = "DeleteUnit";
+
+                CommandParameter param = new CommandParameter("name");
+                param.SetValue(name);
+
+                packet.Parameters.Add(param);
+
+                await connection.SendCommand(packet);
+
+                await block.Wait();
+
+                Packet? responsePacket = block.Packet;
+
+                //ResponsePacket lesen
+            }
+        }
+
+
+    }
+}
---
+++ Connector/Connector/UniverseGroup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flattiverse
+{
+    public class UniverseGroup
+    {
+        private Connection connection;
+
+        private Dictionary<short, Universe> universes;
+
+        private object sync = new object();
+
+        internal UniverseGroup(Connection connection) 
+        {
+            this.connection = connection;
+            universes = new Dictionary<short, Universe>();
+        }
+
+        internal void addUniverse(short id)
+        {
+            lock (sync)
+                universes.Add(0, new Universe(this, connection, id));
+        }
+
+        public bool TryGet(int id, out Universe universe)
+        {
+            if(id > 0 || id > short.MaxValue)
+                throw new ArgumentOutOfRangeException($"Id must be between 0 and {short.MaxValue}.", nameof(id));
+
+            return universes.TryGetValue((short)id, out universe);
+        }
+
+        public IEnumerable<Universe> EnumerateUniverses() 
+        {
+            Dictionary<short, Universe> localUniverses;
+
+            lock (sync)
+                localUniverses= new Dictionary<short, Universe>(universes);
+
+            foreach (KeyValuePair<short, Universe> universeKvP in localUniverses)
+                yield return universeKvP.Value;
+        }
+
+
+    }
+}
