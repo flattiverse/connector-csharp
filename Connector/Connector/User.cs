e51d4622c60de05cbcb2c9053affeba073f40687
Tooasty77 <123006281+Tooasty77@users.noreply.github.com>
2023-01-19 20:32:27 +0100

    Added User

---
+++ Connector/Connector/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flattiverse
+{
+    public class User
+    {
+        public readonly long Id;
+
+        internal User(long id) 
+        {
+            Id = id;
+        }
+    }
+}
