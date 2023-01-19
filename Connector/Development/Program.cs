4194ed5a6c1c11433ed719ba4632a6354ea90116
thorsten <thorsten.paul91@googlemail.com>
2023-01-19 12:41:12 +0100

    test methode

--- Connector/Development/Program.cs
+++ Connector/Development/Program.cs
@@ -1,11 +1,12 @@
 ﻿using Flattiverse;
 using System.Net.WebSockets;
 using System.Text;
+using System.Text.Json;
 
 class Program
 {
     private static async Task Main(string[] args)
-    {
+    {        
         using (Connection connection = new Connection("127.0.0.1" , "TestUser"))
         {
             await connection.ConnectAsync();
@@ -30,32 +31,66 @@ class Program
 
     }
 
-    //public async void test()
-    //{
-    //    var client = new ClientWebSocket();
+    public async void test()
+    {
+        var client = new ClientWebSocket();
 
-    //    await client.ConnectAsync(new Uri("ws://127.0.0.1"), CancellationToken.None);
+        await client.ConnectAsync(new Uri("ws://127.0.0.1"), CancellationToken.None);
 
-    //    ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[4096]);
+        ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[4096]);
 
-    //    while (client.State == WebSocketState.Open)
-    //    {
-    //        Console.Write("Request: ");
+        while (client.State == WebSocketState.Open)
+        {
+            Console.Write("Request: ");
 
-    //        var sendBuffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes("{\"command\":\"OhWhat\",\"id\":\"asd\",\"str\":\"string\"}"));
-    //        await client.SendAsync(sendBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
+            byte[] sendBuffer;
 
-    //        Console.ForegroundColor = ConsoleColor.White;
+            using (MemoryStream stream = new MemoryStream())
+            using (Utf8JsonWriter writer = new Utf8JsonWriter(stream))
+            {
+                writer.WriteStartObject();
+                writer.WriteString("command", "createunit");
+                writer.WriteString("id", "abc");
 
-    //        var result = await client.ReceiveAsync(buffer, CancellationToken.None);
-    //        var message = Encoding.UTF8.GetString(buffer.Array!, 0, result.Count);
-    //        Console.WriteLine(message);
+                writer.WriteStartObject("data");
+                writer.WriteNumber("universe", 0);
 
-    //        Console.ForegroundColor = ConsoleColor.Gray;
+                writer.WriteStartObject("unit");
+                writer.WriteString("name", "test");
+                writer.WriteString("kind", "Sun");
 
-    //        break;
-    //    }
+                writer.WriteStartObject("position");
+                writer.WriteNumber("x", 12);
+                writer.WriteNumber("y", 89);
+                writer.WriteEndObject();
 
-    //    await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
-    //}
+                writer.WriteNumber("radius", 99);
+                writer.WriteNumber("corona", 23);
+                writer.WriteNumber("gravity", 329);
+
+                writer.WriteEndObject();
+
+                writer.WriteEndObject();
+                writer.WriteEndObject();
+                writer.Flush();
+
+                sendBuffer = stream.ToArray();
+            }
+
+            await client.SendAsync(sendBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            var result = await client.ReceiveAsync(buffer, CancellationToken.None);
+            var message = Encoding.UTF8.GetString(buffer.Array!, 0, result.Count);
+            Console.WriteLine(message);
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            break;
+        }
+
+        await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+        return;
+    }
 }
