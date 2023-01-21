ee8245879adb07621a7e87c07c2e32ac85f21dbd
Tooasty77 <123006281+Tooasty77@users.noreply.github.com>
2023-01-21 14:23:41 +0100

    Added player ship

---
+++ Connector/Connector/Units/PlayerShip.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace Flattiverse.Units
+{
+    internal class PlayerShip : Unit
+    {
+        public readonly string UserName;
+
+        public PlayerShip(JsonElement data) : base(data)
+        {
+            if (!Utils.Traverse(data, out UserName, false, "player"))
+                throw new InvalidDataException($"Unit doesn't contain a valid playername property.");
+        }
+    }
+}
