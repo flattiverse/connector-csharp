acf1c2e0baf5d29fb7877badcb1a644f7ef511ca
Tooasty77 <123006281+Tooasty77@users.noreply.github.com>
2023-01-21 19:44:56 +0100

    Added new units asteroid, moon, planet

---
+++ Connector/Connector/Units/Moon.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace Flattiverse.Units
+{
+    public class Moon : Unit
+    {
+        public readonly double Corona;
+
+        public Moon(JsonElement data) : base(data)
+        {
+
+        }
+    }
+}
---
+++ Connector/Connector/Units/Planet.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+using System.Text.Json;
+
+namespace Flattiverse.Units
+{
+    public class Planet : Unit
+    {
+        public readonly double Corona;
+
+        public Planet(JsonElement data) : base(data)
+        {
+
+        }
+    }
+}
