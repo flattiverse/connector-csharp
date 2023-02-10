18c02cc0e7ecbc57e99220b23c46c0f1dfc4b999
Thomas <67207108+Air0r@users.noreply.github.com>
2023-02-11 00:36:59 +0100

    Player und Controllable in RemovedEvent

--- Flattiverse.Connector/Flattiverse.Connector/Events/RemovedUnitEvent.cs
+++ Flattiverse.Connector/Flattiverse.Connector/Events/RemovedUnitEvent.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using Flattiverse.Connector.Accounts;
+using System.Text.Json;
 
 namespace Flattiverse.Connector.Events
 {
@@ -12,10 +13,22 @@ namespace Flattiverse.Connector.Events
         /// The name of the unit, representing his slot in the universegroup's player array.
         /// </summary>
         public readonly string Name;
+        /// <summary>
+        /// The player that controls the unit, if applicable.
+        /// </summary>
+        public readonly Player? Player;
+        /// <summary>
+        /// The controllable of the player, if applicable.
+        /// </summary>
+        public readonly int Controllable;
 
         internal RemovedUnitEvent(UniverseGroup group, JsonElement element) : base(group, element)
         {
             Utils.Traverse(element, out Name, "name");
+            if (Utils.Traverse(element, out int playerID, "player"))
+                Player = group.players[playerID];
+            if (!Utils.Traverse(element, out Controllable, "controllable"))
+                Controllable = -1;
         }
 
         public override EventKind Kind => EventKind.UnitRemoved;
