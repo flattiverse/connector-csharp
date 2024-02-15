5ced750dbebe48161dfa969eee1808fa1527fe5d
CptnChoas <jan-mue@gmx.de>
2024-02-15 22:57:09 +0100

    Added read and write methods

--- Flattiverse.Connector/Flattiverse.Connector/Network/PacketReader.cs
+++ Flattiverse.Connector/Flattiverse.Connector/Network/PacketReader.cs
@@ -19,21 +19,169 @@ class PacketReader
         end = position + packet.Header.Size;
     }
 
+    public sbyte ReadSByte()
+    {
+        Debug.Assert(position + 1 <= end, "Can't read out of bounds.");
+
+        position += 1;
+
+        return Unsafe.As<byte, sbyte>(ref data[position - 1]);
+    }
+
+    public byte ReadByte()
+    {
+        Debug.Assert(position + 1 <= end, "Can't read out of bounds.");
+
+        position += 1;
+
+        return Unsafe.As<byte, byte>(ref data[position - 1]);
+    }
+
+    public short ReadInt16()
+    {
+        Debug.Assert(position + 2 <= end, "Can't read out of bounds.");
+
+        position += 2;
+
+        return Unsafe.As<byte, short>(ref data[position - 2]);
+    }
+
+    public ushort ReadUInt16()
+    {
+        Debug.Assert(position + 2 <= end, "Can't read out of bounds.");
+
+        position += 2;
+
+        return Unsafe.As<byte, ushort>(ref data[position - 2]);
+    }
+
     public int ReadInt32()
     {
         Debug.Assert(position + 4 <= end, "Can't read out of bounds.");
 
         position += 4;
-        
+
         return Unsafe.As<byte, int>(ref data[position - 4]);
     }
 
-    public string ReadString(ushort length)
+    public uint ReadUInt32()
     {
-        Debug.Assert(position + length <= end, "Can't read out of bounds.");
+        Debug.Assert(position + 4 <= end, "Can't read out of bounds.");
 
-        position += length;
+        position += 4;
 
-        return Encoding.ASCII.GetString(data, position - length, length);
+        return Unsafe.As<byte, uint>(ref data[position - 4]);
+    }
+
+    public long ReadInt64()
+    {
+        Debug.Assert(position + 8 <= end, "Can't read out of bounds.");
+
+        position += 8;
+
+        return Unsafe.As<byte, long>(ref data[position - 8]);
+    }
+
+    public ulong ReadUInt64()
+    {
+        Debug.Assert(position + 8 <= end, "Can't read out of bounds.");
+
+        position += 8;
+
+        return Unsafe.As<byte, ulong>(ref data[position - 8]);
+    }
+
+    public double Read1S(double shift)
+    {
+        Debug.Assert(position + 1 <= end, "Can't read out of bounds.");
+
+        position += 1;
+
+        return Unsafe.As<byte, sbyte>(ref data[position - 1]) / shift;
+    }
+
+    public double Read1U(double shift)
+    {
+        Debug.Assert(position + 1 <= end, "Can't read out of bounds.");
+
+        position += 1;
+
+        return Unsafe.As<byte, byte>(ref data[position - 1]) / shift;
+    }
+
+    public double Read2S(double shift)
+    {
+        Debug.Assert(position + 2 <= end, "Can't read out of bounds.");
+
+        position += 2;
+
+        return Unsafe.As<byte, short>(ref data[position - 2]) / shift;
+    }
+
+    public double Read2U(double shift)
+    {
+        Debug.Assert(position + 2 <= end, "Can't read out of bounds.");
+
+        position += 2;
+
+        return Unsafe.As<byte, ushort>(ref data[position - 2]) / shift;
+    }
+
+    public double Read4S(double shift)
+    {
+        Debug.Assert(position + 4 <= end, "Can't read out of bounds.");
+
+        position += 4;
+
+        return Unsafe.As<byte, int>(ref data[position - 4]) / shift;
+    }
+
+    public double Read4U(double shift)
+    {
+        Debug.Assert(position + 4 <= end, "Can't read out of bounds.");
+
+        position += 4;
+
+        return Unsafe.As<byte, uint>(ref data[position - 4]) / shift;
+    }
+
+    public double Read8S(double shift)
+    {
+        Debug.Assert(position + 8 <= end, "Can't read out of bounds.");
+
+        position += 8;
+
+        return Unsafe.As<byte, long>(ref data[position - 8]) / shift;
+    }
+
+    public double Read8U(double shift)
+    {
+        Debug.Assert(position + 8 <= end, "Can't read out of bounds.");
+
+        position += 8;
+
+        return Unsafe.As<byte, ulong>(ref data[position - 8]) / shift;
+    }
+
+    public bool ReadBoolean()
+    {
+        Debug.Assert(position + 1 <= end, "Can't read out of bounds.");
+
+        position += 1;
+
+        return Unsafe.As<byte, byte>(ref data[position - 1]) == 1;
+    }
+
+    public string ReadString()
+    {
+        Debug.Assert(position + 2 <= end, "Can't read out of bounds.");
+
+        short length = Unsafe.As<byte, short>(ref data[position]);
+
+        Debug.Assert(position + 2 + length <= end, "Can't read out of bounds.");
+
+        position += 2 + length;
+
+        return Encoding.UTF8.GetString(Unsafe.As<byte, byte[]>(ref data[position - length]));
     }
 }
--- Flattiverse.Connector/Flattiverse.Connector/Network/PacketWriter.cs
+++ Flattiverse.Connector/Flattiverse.Connector/Network/PacketWriter.cs
@@ -24,28 +24,243 @@ class PacketWriter : IDisposable
         end = position + 1040;
     }
 
+    public void Write(sbyte number)
+    {
+        Debug.Assert(position + 1 < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, sbyte>(ref data[position]) = number;
+
+        position += 1;
+    }
+
+    public void Write(byte number)
+    {
+        Debug.Assert(position + 1 < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, byte>(ref data[position]) = number;
+
+        position += 1;
+    }
+
+    public void Write(short number)
+    {
+        Debug.Assert(position + 2 < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, short>(ref data[position]) = number;
+
+        position += 2;
+    }
+
+    public void Write(ushort number)
+    {
+        Debug.Assert(position + 2 < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, ushort>(ref data[position]) = number;
+
+        position += 2;
+    }
+
     public void Write(int number)
     {
         Debug.Assert(position + 4 < end, "Can't write out of bounds.");
-        
+
         Unsafe.As<byte, int>(ref data[position]) = number;
+
         position += 4;
     }
-    
+
+    public void Write(uint number)
+    {
+        Debug.Assert(position + 4 < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, uint>(ref data[position]) = number;
+
+        position += 4;
+    }
+
+    public void Write(long number)
+    {
+        Debug.Assert(position + 8 < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, long>(ref data[position]) = number;
+
+        position += 8;
+    }
+
+    public void Write(ulong number)
+    {
+        Debug.Assert(position + 8 < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, ulong>(ref data[position]) = number;
+
+        position += 8;
+    }
+
+    /// <summary>
+    /// Writes a double as signed byte.
+    /// </summary>
+    /// <param name="number">The number to write.</param>
+    /// <param name="shift">The shift of the decimal point. e.g. specify 100 to include the first two decimal places.</param>
+    public void Write1S(double number, double shift)
+    {
+        Debug.Assert(position + 1 < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, sbyte>(ref data[position]) = (sbyte)(number * shift + 0.5);
+
+        position += 1;
+    }
+
+    /// <summary>
+    /// Writes a double as unsigned byte.
+    /// </summary>
+    /// <param name="number">The number to write.</param>
+    /// <param name="shift">The shift of the decimal point. e.g. specify 100 to include the first two decimal places.</param>
+    public void Write1U(double number, double shift)
+    {
+        Debug.Assert(position + 1 < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, byte>(ref data[position]) = (byte)(number * shift + 0.5);
+
+        position += 1;
+    }
+
+    /// <summary>
+    /// Writes a double as 2 byte signed short.
+    /// </summary>
+    /// <param name="number">The number to write.</param>
+    /// <param name="shift">The shift of the decimal point. e.g. specify 100 to include the first two decimal places.</param>
+    public void Write2S(double number, double shift)
+    {
+        Debug.Assert(position + 2 < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, short>(ref data[position]) = (short)(number * shift + 0.5);
+
+        position += 2;
+    }
+
+    /// <summary>
+    /// Writes a double as 2 byte unsigned short.
+    /// </summary>
+    /// <param name="number">The number to write.</param>
+    /// <param name="shift">The shift of the decimal point. e.g. specify 100 to include the first two decimal places.</param>
+    public void Write2U(double number, double shift)
+    {
+        Debug.Assert(position + 2 < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, ushort>(ref data[position]) = (ushort)(number * shift + 0.5);
+
+        position += 2;
+    }
+
     /// <summary>
     /// Writes a double as 4 byte signed integer.
     /// </summary>
     /// <param name="number">The number to write.</param>
-    /// <param name="shift">The shift of the decimal point. If you want to store 2 decimal places,
-    /// you need to specify 100 here.</param>
+    /// <param name="shift">The shift of the decimal point. e.g. specify 100 to include the first two decimal places.</param>
     public void Write4S(double number, double shift)
     {
         Debug.Assert(position + 4 < end, "Can't write out of bounds.");
-        
+
         Unsafe.As<byte, int>(ref data[position]) = (int)(number * shift + 0.5);
+
         position += 4;
     }
-    
+
+    /// <summary>
+    /// Writes a double as 4 byte unsigned integer.
+    /// </summary>
+    /// <param name="number">The number to write.</param>
+    /// <param name="shift">The shift of the decimal point. e.g. specify 100 to include the first two decimal places.</param>
+    public void Write4U(double number, double shift)
+    {
+        Debug.Assert(position + 4 < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, uint>(ref data[position]) = (uint)(number * shift + 0.5);
+
+        position += 4;
+    }
+
+    /// <summary>
+    /// Writes a double as 8 byte signed long.
+    /// </summary>
+    /// <param name="number">The number to write.</param>
+    /// <param name="shift">The shift of the decimal point. e.g. specify 100 to include the first two decimal places.</param>
+    public void Write8S(double number, double shift)
+    {
+        Debug.Assert(position + 8 < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, long>(ref data[position]) = (long)(number * shift + 0.5);
+
+        position += 8;
+    }
+
+    /// <summary>
+    /// Writes a double as 8 byte unsigned long.
+    /// </summary>
+    /// <param name="number">The number to write.</param>
+    /// <param name="shift">The shift of the decimal point. e.g. specify 100 to include the first two decimal places.</param>
+    public void Write8U(double number, double shift)
+    {
+        Debug.Assert(position + 8 < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, ulong>(ref data[position]) = (ulong)(number * shift + 0.5);
+
+        position += 8;
+    }
+
+    /// <summary>
+    /// Writes a boolean value.
+    /// </summary>
+    /// <param name="value">The boolean to write.</param>
+    public void Write(bool value)
+    {
+        Debug.Assert(position + 1 < end, "Can't write out of bounds.");
+
+        if (value)
+            Unsafe.As<byte, byte>(ref data[position + 1]) = 1;
+        else
+            Unsafe.As<byte, byte>(ref data[position + 1]) = 0;
+
+        position += 1;
+    }
+
+    /// <summary>
+    /// Writes a string using UTF-8.
+    /// </summary>
+    /// <param name="text">The text to write.</param>
+    public void Write(string text)
+    {
+        byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+        Debug.Assert(position + 2 + bytes.Length < end, "Can't write out of bounds.");
+
+        Unsafe.As<byte, short>(ref data[position]) = (short)bytes.Length;
+        Unsafe.CopyBlock(ref data[position + 2], ref bytes[0], (uint)bytes.Length);
+
+        position += 2 + bytes.Length;
+    }
+
+    internal void WriteNullable(byte? number)
+    {
+        if (number.HasValue)
+        {
+            Debug.Assert(position + 2 < end, "Can't write out of bounds.");
+
+            Unsafe.As<byte, byte>(ref data[position]) = 1;
+            Unsafe.As<byte, byte>(ref data[position + 1]) = number.Value;
+
+            position += 2;
+        }
+        else
+        {
+            Debug.Assert(position + 1 < end, "Can't write out of bounds.");
+
+            Unsafe.As<byte, byte>(ref data[position]) = 1;
+
+            position += 1;
+        }
+    }
+
     public void Dispose()
     {
         packet.Header.Size = (ushort)(position - packet.Offset);
