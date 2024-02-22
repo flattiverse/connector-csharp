346d1e6957925755e2160e7c63696fd014b71a53
Jonas <Jonas>
2024-02-22 10:33:25 +0100

    Added Left,Top,Right,Bottom to RegionConfig

--- Flattiverse.Connector/Flattiverse.Connector/Hierarchy/RegionConfig.cs
+++ Flattiverse.Connector/Flattiverse.Connector/Hierarchy/RegionConfig.cs
@@ -8,6 +8,10 @@ namespace Flattiverse.Connector.Hierarchy
         public double StartPropability;
         public double RespawnPropability;
         public bool Protected;
+        public double Left;
+        public double Top;
+        public double Right;
+        public double Bottom;
 
         private RegionConfig()
         {
@@ -15,6 +19,10 @@ namespace Flattiverse.Connector.Hierarchy
             StartPropability = 0;
             RespawnPropability = 0;
             Protected = false;
+            Left = 0;
+            Top = 0;
+            Right = 0;
+            Bottom = 0;
         }
 
         internal RegionConfig(RegionConfig region)
@@ -23,6 +31,10 @@ namespace Flattiverse.Connector.Hierarchy
             StartPropability = region.StartPropability;
             RespawnPropability = region.RespawnPropability;
             Protected = region.Protected;
+            Left = region.Left;
+            Top = region.Top;
+            Right = region.Right;
+            Bottom = region.Bottom;
         }
 
         internal RegionConfig(PacketReader reader)
@@ -31,6 +43,10 @@ namespace Flattiverse.Connector.Hierarchy
             StartPropability = reader.Read2U(100);
             RespawnPropability = reader.Read2U(100);
             Protected = reader.ReadBoolean();
+            Left = reader.Read2U(100);
+            Top = reader.Read2U(100);
+            Right = reader.Read2U(100);
+            Bottom = reader.Read2U(100);
         }
 
         internal static RegionConfig Default => new RegionConfig();
@@ -41,6 +57,10 @@ namespace Flattiverse.Connector.Hierarchy
             writer.Write2U(StartPropability, 100);
             writer.Write2U(RespawnPropability, 100);
             writer.Write(Protected);
+            writer.Write2U(Left, 100);
+            writer.Write2U(Top, 100);
+            writer.Write2U(Right, 100);
+            writer.Write2U(Bottom, 100);
         }
     }
 }
