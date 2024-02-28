261a893a6e7b73006af9ffd904e19cc5dcfca561
Dominik Frisch <7753812+h3xc4t@users.noreply.github.com>
2024-02-28 16:32:30 +0100

    added game mode docs

--- Flattiverse.Connector/Flattiverse.Connector/GameMode.cs
+++ Flattiverse.Connector/Flattiverse.Connector/GameMode.cs
@@ -1,10 +1,25 @@
 ﻿namespace Flattiverse.Connector
 {
+    /// <summary>
+    /// The game mode of a galaxy.
+    /// </summary>
     public enum GameMode
     {
+        /// <summary>
+        /// In this game mode players try to complete mission objectives.
+        /// </summary>
         Mission,
+        /// <summary>
+        /// In this game mode players try to destroy the enemy flag.
+        /// </summary>
         STF,
+        /// <summary>
+        /// In this game mode players fight over control points.
+        /// </summary>
         Domination,
+        /// <summary>
+        /// In this game mode players try to get the fastest time on a track.
+        /// </summary>
         Race
     }
 }
