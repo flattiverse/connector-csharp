ac676cf0d3ab78b85c6b60d511a4fd02fa2b2549
Dominik Frisch <7753812+h3xc4t@users.noreply.github.com>
2024-02-28 15:44:16 +0100

    added docs for EventKind

--- Flattiverse.Connector/Flattiverse.Connector/Events/EventKind.cs
+++ Flattiverse.Connector/Flattiverse.Connector/Events/EventKind.cs
@@ -5,20 +5,59 @@
     /// </summary>
     public enum EventKind : byte
     {
+        /// <summary>
+        /// A unit was added to the universe.
+        /// </summary>
         UnitAdded = 0x00,
+
+        /// <summary>
+        /// You received an update for a unit.
+        /// </summary>
         UnitUpdated = 0x01,
+
+        /// <summary>
+        /// The unit went ouside scanning cone.
+        /// </summary>
         UnitVanished = 0x02,
 
+        /// <summary>
+        /// A player joined the universe.
+        /// </summary>
         JoinedPlayer = 0x04,
+
+        /// <summary>
+        /// A player left the universe.
+        /// </summary>
         PartedPlayer = 0x05,
-        
+
+        /// <summary>
+        /// Someone registerd a ship in the cluster.
+        /// </summary>
         JoinedControllable = 0x08,
+
+        /// <summary>
+        /// Someone unregistered a ship in the cluster.
+        /// </summary>
         PartedControllable = 0x09,
         
+        /// <summary>
+        /// Someone wrote a message in the chat.
+        /// </summary>
         PlayerChat = 0x0C,
+
+        /// <summary>
+        /// Someone wrote a message in the team chat.
+        /// </summary>
         TeamChat = 0x0D,
+
+        /// <summary>
+        /// Someone wrote a message in the galaxy chat.
+        /// </summary>
         GalaxyChat = 0x0E,
         
+        /// <summary>
+        /// The server processed a tick.
+        /// </summary>
         GalaxyTick = 0xFF
     }
 }
