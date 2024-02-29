703e2ec2a0b9e83db20051a1486ba87161761f94
Dominik Frisch <7753812+h3xc4t@users.noreply.github.com>
2024-02-29 14:28:34 +0100

    added docs to region

--- Flattiverse.Connector/Flattiverse.Connector/Hierarchy/Region.cs
+++ Flattiverse.Connector/Flattiverse.Connector/Hierarchy/Region.cs
@@ -2,9 +2,20 @@
 
 namespace Flattiverse.Connector.Hierarchy
 {
+    /// <summary>
+    /// This is a defined area in a cluster which then can be configured for special game rules
+    /// like a starting location or a region with a special name which will be shown to the player via GameMessage.
+    /// </summary>
     public class Region : INamedUnit
     {
+        /// <summary>
+        /// The galaxy this region is in.
+        /// </summary>
         public readonly Galaxy Galaxy;
+
+        /// <summary>
+        /// The cluster this region is in.
+        /// </summary>
         public readonly Cluster Cluster;
 
         private byte id;
@@ -12,6 +23,9 @@ namespace Flattiverse.Connector.Hierarchy
 
         private bool isActive;
 
+        /// <summary>
+        /// This flag indicates if this region is still part of the simulation.
+        /// </summary>
         public bool IsActive => isActive;
 
         internal Region(Galaxy galaxy, Cluster cluster, byte id, PacketReader reader)
@@ -24,11 +38,22 @@ namespace Flattiverse.Connector.Hierarchy
             config = new RegionConfig(reader);
         }
 
+        /// <summary>
+        /// The ID of the region. This is unique in the cluster.
+        /// </summary>
         public int ID => id;
+
         /// <summary>
         /// The name of the region.
         /// </summary>
+        /// <remarks>
+        /// SAFETY: Make sure this is unique in the cluster, since can be addressed via name.
+        /// </remarks>
         public string Name => config.Name;
+
+        /// <summary>
+        /// The configuration of this region.
+        /// </summary>
         public RegionConfig Config => config;
 
         /// <summary>
