6860d0056e168a93af7e92845298f1333f46bab8
Dominik Frisch <7753812+h3xc4t@users.noreply.github.com>
2024-02-29 14:20:55 +0100

    added doc to PlayerKind

--- Flattiverse.Connector/Flattiverse.Connector/Hierarchy/PlayerKind.cs
+++ Flattiverse.Connector/Flattiverse.Connector/Hierarchy/PlayerKind.cs
@@ -1,9 +1,23 @@
 ﻿namespace Flattiverse.Connector.Hierarchy
 {
+    /// <summary>
+    /// The type/kind/role of a player.
+    /// </summary>
     public enum PlayerKind
     {
+        /// <summary>
+        /// A regular player can spawn ships and control them.
+        /// </summary>
         Player = 0x01,
+
+        /// <summary>
+        /// A spectator can only watch the game.
+        /// </summary>
         Spectator = 0x02,
+
+        /// <summary>
+        /// Functions similar to a spectator but can also control the game.
+        /// </summary>
         Admin = 0x04,
     }
 }
