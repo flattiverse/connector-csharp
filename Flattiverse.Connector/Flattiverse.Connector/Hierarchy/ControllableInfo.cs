0f4a26752028de6c28d437f25242fdac8243be22
Dominik Frisch <7753812+h3xc4t@users.noreply.github.com>
2024-02-29 11:26:20 +0100

    added controllableinfo docs

--- Flattiverse.Connector/Flattiverse.Connector/Hierarchy/ControllableInfo.cs
+++ Flattiverse.Connector/Flattiverse.Connector/Hierarchy/ControllableInfo.cs
@@ -2,14 +2,25 @@
 
 namespace Flattiverse.Connector.Hierarchy
 {
+    /// <summary>
+    /// This class contains information about a controllable.
+    /// You will receive this information when you join a galaxy and when a controllable is updated.
+    /// </summary>
     public class ControllableInfo : INamedUnit
     {
         private Galaxy galaxy;
 
+        /// <summary>
+        /// A local unique identifier for the controllable. This is not the same as the player id.
+        /// Used to differentiate between multiple controllables of the same player.
+        /// </summary>
         public readonly int Id;
 
         private readonly string name;
 
+        /// <summary>
+        /// If the controllable is part of the enemy team, you will receive limited information about it.
+        /// </summary>
         public readonly bool Reduced;
 
         private ShipDesign shipDesign;
@@ -34,23 +45,94 @@ namespace Flattiverse.Connector.Hierarchy
 
         private bool active;
 
+        /// <summary>
+        /// ShipDesign contains the basis stats of the ship.
+        /// </summary>
+        /// <remarks>
+        /// Upgrades can be applied, but it wont be reflected in this object.
+        /// </remarks>
         public ShipDesign ShipDesign => shipDesign;
+
+        /// <summary>
+        /// The player that owns this controllable.
+        /// </summary>
         public Player Player => player;
+
+        /// <summary>
+        /// The hull level of the controllable.
+        /// If it reaches 0, the controllable is destroyed.
+        /// The player can call continue to respawn the controllable.
+        /// </summary>
         public double Hull => hull;
+
+        /// <summary>
+        /// The maximum hull level of the controllable.
+        /// </summary>
+        /// <seealso cref="Hull"/>
         public double HullMax => hullMax;
+
+        /// <summary>
+        /// The current shield value of the ship.
+        /// It will be depleted before the hull is damaged.
+        /// </summary>
+        /// <remarks>
+        /// Depending on a value in ShipDesign you may need Ion to recharge the shields.
+        /// </remarks>
         public double Shields => shields;
+
+        /// <summary>
+        /// The maximum shield level of the controllable.
+        /// </summary>
+        /// <seealso cref="Shields"/>
         public double ShieldsMax => shieldsMax;
    
+        /// <summary>
+        /// The current energy value of the ship.
+        /// It can be gained via sections/coronas around suns or via the energy reactor of stations.
+        /// </summary>
+        /// <remarks>
+        /// Energy is used for the thruster and the weapons.
+        /// It can also be used to repair the hull or recharge the Shields.
+        /// </remarks>
         public double Energy => energy;
+
+        /// <summary>
+        /// The maximum energy value of the ship.
+        /// </summary>
+        /// <seealso cref="Energy" />
         public double EnergyMax => energyMax;
  
+         /// <summary>
+        /// The current ion value of the ship.
+        /// Ion is used to recharge the shields.
+        /// </summary>
+        /// <remarks>
+        /// Ios can be gained via corona secions configured in the map.
+        /// </remarks>
         public double Ion => ion;
+
+        /// <summary>
+        /// The maximum ion value of the ship.
+        /// </summary>
+        /// <seealso cref="Ion" />
         public double IonMax => ionMax;
 
+        /// <summary>
+        /// This flag indicates if the controllable can receive commands other than continue and dispose.
+        /// </summary>
         public bool IsAlive => alive;
-        
-        public bool Active => active;
 
+        /// <summary>
+        /// This flag indicates if the controllable is able to be interacted with.
+        /// </summary>
+        /// <remarks>
+        /// If it is inactive and you try to interact with it, you will get a GameException.
+        /// </remarks>
+        public bool IsActive => active;
+
+        /// <summary>
+        /// The name of the controllable.
+        /// </summary>
         public string Name => name;
 
         internal ControllableInfo(Galaxy galaxy, Player player, PacketReader reader, int id, bool reduced)
